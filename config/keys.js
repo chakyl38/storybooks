695401aa91c148e008e3db9443d7775cd4c51365
Kyle Chapman <kylechapman09@gmail.com>
2018-06-01 17:08:31 -0500

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://chakyl38:Comanche1@ds237660.mlab.com:37660/storybooks-dev',
+    googleClientID: '447810524919-koahfei3l83poohh5vnmkiddsbiicjt5.apps.googleusercontent.com',
+    googleClientSecret: 'xFmvIa-ITXiCQzYycm7m8Za0'
+}
